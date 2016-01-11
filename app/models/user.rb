6daea33b23062d67c7927747d9b3d6e2cc4dde59
moinudeen <moinu.95@gmail.com>
2016-01-11 23:06:03 +0530

    Devise gem added

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :mobile_number, format: { with: /\d{10}/, message: "bad format" }
+end
