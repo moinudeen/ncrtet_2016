702b9f144bd5d454319a22cdb74525d16975f4c3
Vijay Chandar <reachvijaychandar@gmail.com>
2016-01-30 10:32:47 +0530

    permitter added in controlller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(user)
+    call_for_papers_path
+  end
+  def after_sign_out_path_for(user)
+    home_path
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me,:first_name, :last_name, :mobile_number, :college_name, :designation) }
+  end
+
 end
