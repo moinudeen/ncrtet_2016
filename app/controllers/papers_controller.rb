b079b8d310dd5692bdba1467c130f3999c54bd3c
Vijay Chandar <reachvijaychandar@gmail.com>
2016-02-24 21:52:57 +0530

    Secrets edited

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -44,7 +44,7 @@ class PapersController < ApplicationController
   def update
     respond_to do |format|
       if @paper.update(paper_params)
-        format.html { redirect_to @paper, notice: 'Paper was successfully updated.' }
+        format.html { redirect_to papers_path, notice: 'Paper was successfully updated.' }
         format.json { render :show, status: :ok, location: @paper }
       else
         format.html { render :edit }
